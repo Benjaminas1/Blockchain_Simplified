2be7d3cf14853ea47b7599b232bd0b0926c9df46
Benjaminas1 <ben.racas@gmail.com>
2021-10-14 21:56:08 +0300

    block mining and generating implemented

--- main.cpp
+++ main.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 
 int main(){
-    vector<userStruct> users = generateUsers(1000);
-    vector<transactionStruct> transactions = generateTransactions(10000, users);
-    putTransactionsToBlock(transactions);
+    int difficultyTarget = 4;
+    vector<userClass> users = generateUsers(1000);
+    vector<transactionClass> transactions = generateTransactions(10000, users);
+
+    blockchainClass blockchain;
+    // Find new block
+    while(true){
+        unsigned int number = generateRandomNumber(0, UINT_MAX);
+        string hashedNumber = hashString(to_string(number));
+        int zerosFound = 0;
+
+        for(int i=0; i<difficultyTarget; i++){
+            if(hashedNumber[i] == '0') zerosFound++;
+        }
+
+        if(zerosFound == difficultyTarget){
+            blockchain.blocks.push_back(generateBlock(transactions, number));
+        }
+    }
+
 }
