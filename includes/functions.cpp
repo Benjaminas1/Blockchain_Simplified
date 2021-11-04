58e702c4c0555f94ecdbd1fc8f5aaf333f145fdf
Benjaminas <ben.racas@gmail.com>
2021-11-04 22:39:46 +0200

    changed block version

--- includes/functions.cpp
+++ includes/functions.cpp
@@ -133,7 +133,7 @@ blockClass generateBlock(vector<transactionClass> &transactions)
 
         transactionsVector.push_back(transactions[i]);
     }
-    blockClass block(transactionsVector, "v0.1", getMerkleRoot(transactions));
+    blockClass block(transactionsVector, "v0.2", getMerkleRoot(transactions));
 
     return block;
 }
